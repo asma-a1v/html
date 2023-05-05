329a711608c5499083a7ff26c7d9092b6acb6eb1
ASMA <fujitat.sk.147@gmail.com>
2023-05-05 23:48:08 +0900

    Update script.js

--- 5/script.js
+++ 5/script.js
@@ -5,128 +5,125 @@ let activeBox = null;
 let offsetX = 0;
 let offsetY = 0;
 
-carInfoForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const identifier = document.getElementById('identifier').value;
-  const hour = document.getElementById('hour').value;
-  const minute = document.getElementById('minute').value;
-  const carType1 = document.getElementById('car-type1').value;
-  const carType2 = document.getElementById('car-type2').value;
-  const carType3 = document.getElementById('car-type3').value;
-  const plateRegion = document.getElementById('plate-region').value;
-  const plateNumber = document.getElementById('plate-number').value;
-  const plateHiragana = document.getElementById('plate-hiragana').value;
-  const plateSerial = document.getElementById('plate-serial').value;
-
-  createDraggableBox(identifier, `${hour}:${minute}`, carType1, carType2, carType3, plateRegion, plateNumber, plateHiragana, plateSerial);
+const savedData = JSON.parse(localStorage.getItem('draggableBoxes')) || [];
+savedData.forEach(boxData => {
+  createDraggableBox(
+    boxData.identifier,
+    boxData.time,
+    boxData.carType1,
+    boxData.carType2,
+    boxData.carType3,
+    boxData.plateRegion,
+    boxData.plateNumber,
+    boxData.plateHiragana,
+    boxData.plateSerial,
+    boxData.position
+  );
 });
 
-function createDraggableBox(identifier, time, carType1, carType2, carType3, plateRegion, plateNumber, plateHiragana, plateSerial) {
-  const draggableBox = document.createElement('div');
-  draggableBox.classList.add('draggable-box');
+  carInfoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-  const identifierElement = document.createElement('div');
-  identifierElement.classList.add('identifier');
-  identifierElement.textContent = identifier;
-  draggableBox.appendChild(identifierElement);
+    const identifier = document.getElementById('identifier').value;
+    const hour = document.getElementById('hour').value;
+    const minute = document.getElementById('minute').value;
+    const carType1 = document.getElementById('car-type1').value;
+    const carType2 = document.getElementById('car-type2').value;
+    const carType3 = document.getElementById('car-type3').value;
+    const plateRegion = document.getElementById('plate-region').value;
+    const plateNumber = document.getElementById('plate-number').value;
+    const plateHiragana = document.getElementById('plate-hiragana').value;
+    const plateSerial = document.getElementById('plate-serial').value;
 
-  const removeBtn = document.createElement('div');
-  removeBtn.classList.add('remove-btn');
-  removeBtn.textContent = '✕';
-  draggableBox.appendChild(removeBtn);
-
-  removeBtn.addEventListener('click', () => {
-    boxContainer.removeChild(draggableBox);
-    saveDataToLocalStorage();
+    createDraggableBox(identifier, `${hour}:${minute}`, carType1, carType2, carType3, plateRegion, plateNumber, plateHiragana, plateSerial);
   });
 
-  const boxContent = document.createElement('div');
-  boxContent.classList.add('box-content');
+  function createDraggableBox(identifier, time, carType1, carType2, carType3, plateRegion, plateNumber, plateHiragana, plateSerial, position = null) {
+    
+    const draggableBox = document.createElement('div');
+    draggableBox.classList.add('draggable-box');
+  
+    const identifierElement = document.createElement('div');
+    identifierElement.classList.add('identifier');
+    identifierElement.textContent = identifier;
+    draggableBox.appendChild(identifierElement);
+  
+    const removeBtn = document.createElement('div');
+    removeBtn.classList.add('remove-btn');
+    removeBtn.textContent = '✕';
+    draggableBox.appendChild(removeBtn);
+  
+    // 削除ボタンのクリックイベントリスナー
+    removeBtn.addEventListener('click', () => {
+      boxContainer.removeChild(draggableBox);
+    });
+  
+    const boxContent = document.createElement('div');
+    boxContent.classList.add('box-content');
+  
+    const timeElement = document.createElement('div');
+    timeElement.classList.add('info-line');
+    timeElement.textContent = time;
+    boxContent.appendChild(timeElement);
+  
+    const carTypeElement = document.createElement('div');
+    carTypeElement.classList.add('info-line');
+    carTypeElement.innerHTML = `${carType1} ${carType2}<br>${carType3}`;
+    boxContent.appendChild(carTypeElement);
+  
+    const plateInfoElement = document.createElement('div');
+    plateInfoElement.classList.add('plate-info');
+    plateInfoElement.innerHTML = `${plateRegion} ${plateNumber}<br>${plateHiragana} ${plateSerial}`;
+    boxContent.appendChild(plateInfoElement);
 
-  const timeElement = document.createElement('div');
-  timeElement.classList.add('info-line');
-  timeElement.textContent = time;
-  boxContent.appendChild(timeElement);
-
-  const carTypeElement = document.createElement('div');
-  carTypeElement.classList.add('info-line');
-  carTypeElement.innerHTML = `${carType1} ${carType2}<br>${carType3}`;
-  boxContent.appendChild(carTypeElement);
-
-  const plateInfoElement = document.createElement('div');
-  plateInfoElement.classList.add('plate-info');
-  plateInfoElement.innerHTML = `${plateRegion} ${plateNumber}<br>${plateHiragana} ${plateSerial}`;
-  boxContent.appendChild(plateInfoElement);
-
-  draggableBox.appendChild(boxContent);
-
-  boxContainer.appendChild(draggableBox);
-
-  draggableBox.addEventListener('mousedown', (e) => {
-    activeBox = draggableBox;
-    offsetX = e.clientX - draggableBox.getBoundingClientRect().left;
-    offsetY = e.clientY - draggableBox.getBoundingClientRect().top;
-  });
+    if (position) {
+      draggableBox.style.left = position.left;
+      draggableBox.style.top = position.top;
+    }
+  
+    draggableBox.appendChild(boxContent);
+  
+    boxContainer.appendChild(draggableBox);
+  
+    draggableBox.addEventListener('mousedown', (e) => {
+      activeBox = draggableBox;
+      offsetX = e.clientX - draggableBox.getBoundingClientRect().left;
+      offsetY = e.clientY - draggableBox.getBoundingClientRect().top;
+    });
 }
+  
 
 document.addEventListener('mousemove', (e) => {
-  if (!activeBox) return;
-  activeBox.style.left = `${e.clientX - offsetX}px`;
-  activeBox.style.top = `${e.clientY - offsetY}px`;
+    if (!activeBox) return;
+    activeBox.style.left = `${e.clientX - offsetX}px`;
+    activeBox.style.top = `${e.clientY - offsetY}px`;
+});
+
+document.addEventListener('mouseup', () => {
+    activeBox = null;
 });
 
 document.addEventListener('mouseup', () => {
   if (!activeBox) return;
-  saveDataToLocalStorage();
-  activeBox = null;
-});
 
-function saveDataToLocalStorage() {
   const draggableBoxesData = Array.from(boxContainer.children).map((box) => {
-    const timeElement = box.querySelector('.info-line');
-    const carTypeElement = timeElement.nextElementSibling;
-    const plateInfoElement = carTypeElement.nextElementSibling;
     return {
       identifier: box.querySelector('.identifier').textContent,
-      time: timeElement.textContent,
-      carType1: carTypeElement.textContent.split(' ')[0],
-      carType2: carTypeElement.textContent.split(' ')[1],
-      carType3: carTypeElement.nextElementSibling.textContent,
-      plateRegion: plateInfoElement.textContent.split(' ')[0],
-      plateNumber: plateInfoElement.textContent.split(' ')[1],
-      plateHiragana: plateInfoElement.nextElementSibling.textContent.split(' ')[0],
-      plateSerial: plateInfoElement.nextElementSibling.textContent.split(' ')[1],
+      time: box.querySelector('.info-line').textContent,
+      carType1: box.querySelector('.info-line').nextElementSibling.textContent.split(' ')[0],
+      carType2: box.querySelector('.info-line').nextElementSibling.textContent.split(' ')[1],
+      carType3: box.querySelector('.info-line').nextElementSibling.nextElementSibling.textContent,
+      plateRegion: box.querySelector('.plate-info').textContent.split(' ')[0],
+      plateNumber: box.querySelector('.plate-info').textContent.split(' ')[1],
+      plateHiragana: box.querySelector('.plate-info').nextElementSibling.textContent.split(' ')[0],
+      plateSerial: box.querySelector('.plate-info').nextElementSibling.textContent.split(' ')[1],
       position: {
         left: box.style.left,
         top: box.style.top
       }
     };
   });
-  localStorage.setItem('draggableBoxesData', JSON.stringify(draggableBoxesData));
-}
 
-
-function loadDataFromLocalStorage() {
-  const draggableBoxesData = JSON.parse(localStorage.getItem('draggableBoxesData'));
-  if (draggableBoxesData) {
-    draggableBoxesData.forEach((boxData) => {
-      const draggableBox = createDraggableBox(
-        boxData.identifier,
-        boxData.time,
-        boxData.carType1,
-        boxData.carType2,
-        boxData.carType3,
-        boxData.plateRegion,
-        boxData.plateNumber,
-        boxData.plateHiragana,
-        boxData.plateSerial
-      );
-      draggableBox.style.left = boxData.position.left;
-      draggableBox.style.top = boxData.position.top;
-    });
-  }
-}
-
-// ページ読み込み時にローカルストレージからデータを読み込む
-loadDataFromLocalStorage();
+  localStorage.setItem('draggableBoxes', JSON.stringify(draggableBoxesData));
+  activeBox = null;
+});
