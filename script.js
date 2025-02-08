ef1822235f632972712e82780388d2f1acb5e9bd
ASMA <66177648+asma-a1v@users.noreply.github.com>
2025-02-09 03:44:33 +0900

    add

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ function updateWindowSize() {
     document.getElementById('height').textContent = height;
 }
 
-// DOMが完全に読み込まれた後に実行
-document.addEventListener('DOMContentLoaded', function() {
+	// DOMが完全に読み込まれた後に実行
+	document.addEventListener('DOMContentLoaded', function() {
     // 初期サイズを表示
     updateWindowSize();
 
