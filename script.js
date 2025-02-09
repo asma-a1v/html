65f611e6d841ad5090f5f355ffc4bc28dda87ccb
ASMA <66177648+asma-a1v@users.noreply.github.com>
2025-02-09 23:34:04 +0900

    ウィンドウサイズ表示機能を更新し、ボタンスタイルを追加。不要なファイルを削除。

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function window_load() {
-	var sW,sH,s;
-	sW = window.innerWidth;
-	sH = window.innerHeight;
-
-	s = "ウィンドウサイズ：横幅 = " + sW + " / 高さ = " + sH;
-	document.getElementById("winsize2").innerHTML = s;
-}
-
 // ウィンドウサイズを取得して表示する関数
 function updateWindowSize() {
     const width = window.innerWidth;
